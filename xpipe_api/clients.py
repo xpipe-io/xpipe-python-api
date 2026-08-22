776d51080c8f180f7b246bfef0c35992c5292516
Christopher Schnick <crschnick@xpipe.io>
2026-08-22 16:08:55 +0200

    Bump version

--- xpipe_api/clients.py
+++ xpipe_api/clients.py
@@ -20,7 +20,7 @@ class Client:
     base_url: str
     raise_errors: bool
     session: Optional[str] = None
-    min_version: Version = Version("17.0")
+    min_version: Version = Version("24.0")
 
     def __init__(
         self, token: Optional[str] = None, base_url: Optional[str] = None, ptb: bool = False, raise_errors: bool = True
